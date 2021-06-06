3611e09fb73907486241ee3ce7cd44a3c45e147d
Maciej Wiatr <maciekwiatr17@gmail.com>
2021-06-06 12:33:52 +0200

    add borderColor to theme

--- src/themes.js
+++ src/themes.js
@@ -2,12 +2,14 @@ const lightTheme = {
 	backgroundColor: "#deeeea",
 	textBase: "#231e23",
 	textPrimary: "#bf1363",
+	borderColor: "lightgray",
 };
 
 const darkTheme = {
 	backgroundColor: "#231e23",
 	textBase: "#deeeea",
 	textPrimary: "#bf1363",
+	borderColor: "#505050",
 };
 
 export { lightTheme, darkTheme };
