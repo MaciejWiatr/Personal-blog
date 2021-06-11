0989154968ff33c5d309f5a033dbca11532a7cb3
Maciej Wiatr <maciekwiatr17@gmail.com>
2021-06-11 23:43:03 +0200

    Remove redundant useDarkMode hook

--- src/hooks/useDarkMode.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { ColormodeContext } from "../contexts/ThemeContext";
-
-const useDarkMode = () => {
-	return useContext(ColormodeContext);
-};
-
-export default useDarkMode;
