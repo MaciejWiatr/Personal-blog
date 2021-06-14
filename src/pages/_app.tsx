212c24eb2396cf1b0b35dda9bec0c9fa65d02618
Maciej Wiatr <maciekwiatr17@gmail.com>
2021-06-14 14:15:13 +0200

    Make mousefollower client-only

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { appWithTranslation } from 'next-i18next';
+import dynamic from 'next/dynamic';
 import { useRef } from 'react';
 import '../../styles/globals.css';
 import ColormodeProvider from '../contexts/ThemeContext';
 import { AppWrapper } from '@components/Layout';
-import { MouseFollower } from '@components/Mouse';
+
+const MouseFollower = dynamic(
+    () => import('../components/Mouse').then((mod) => mod.MouseFollower),
+    { ssr: false }
+);
 
 function MyApp({ Component, pageProps }) {
     const appRef = useRef(null);
