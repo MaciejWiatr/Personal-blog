98d93df6a927ea8ca1973cdd44dde450aa1c57f5
Maciej Wiatr <maciekwiatr17@gmail.com>
2021-06-16 17:47:59 +0200

    Add missing dependencies

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { i18n } = require('./next-i18next.config');
-const env = process.env.NODE_ENV;
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE,
+    enabled: process.env.ANALYZE == 'true',
 });
+
 const withPreact = require('next-plugin-preact');
 
 module.exports = withBundleAnalyzer(
