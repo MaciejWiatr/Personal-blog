6908e34736f21560e083fa96c52ba08d61a7c9b8
Maciej Wiatr <maciekwiatr17@gmail.com>
2021-08-10 17:21:44 +0200

    Fix unused import errors

--- src/components/Layout/BaseLayout.tsx
+++ src/components/Layout/BaseLayout.tsx
@@ -1,12 +1,9 @@
 import styled from '@emotion/styled';
 import { FC } from 'react';
-import BottomNav from '../Navigation/BottomNav';
 import NavBar from '../Navigation/Navbar';
-import useMediaQuery from '@hooks/useMediaQuery';
 import mq from '@shared/utils/mediaQuery';
 
 const BaseLayout: FC = ({ children }) => {
-
     return (
         <>
             <MainContainer>
@@ -18,25 +15,25 @@ const BaseLayout: FC = ({ children }) => {
 };
 
 const MainContainer = styled.div`
-  background-color: ${({theme}) => theme.backgroundColor};
-  color: ${({theme}) => theme.textBase};
-  min-height: 100vh;
-  padding: 5rem 15rem;
-  @media ${mq('lg')} {
-    padding-left: 7rem;
-    padding-right: 7rem;
-  }
+    background-color: ${({ theme }) => theme.backgroundColor};
+    color: ${({ theme }) => theme.textBase};
+    min-height: 100vh;
+    padding: 5rem 15rem;
+    @media ${mq('lg')} {
+        padding-left: 7rem;
+        padding-right: 7rem;
+    }
 
-  @media ${mq('md')} {
-    padding-left: 5rem;
-    padding-right: 5rem;
-  }
+    @media ${mq('md')} {
+        padding-left: 5rem;
+        padding-right: 5rem;
+    }
 
-  @media ${mq('sm')} {
-    padding-left: 1rem;
-    padding-right: 1rem;
-    padding-top: 5rem;
-  }
+    @media ${mq('sm')} {
+        padding-left: 1rem;
+        padding-right: 1rem;
+        padding-top: 5rem;
+    }
 `;
 
 export default BaseLayout;
